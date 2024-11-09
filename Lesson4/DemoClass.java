58a2485e426ace1fbdaa5c16924dc7564db61e3b
Olga <aqa_olga.bobryk@mail.com>
2024-11-09 19:22:03 +0300

    lesson4

---
+++ Lesson4/DemoClass.java
@@ -0,0 +1,41 @@
+package Lesson4;
+
+public class DemoClass {
+    public static void main(String[] args) {
+
+
+        Employee emp = new Employee();
+        emp.name = "Васильев Василий Васильевич";
+        emp.age = 31;
+        emp.salary = 1000;
+        emp.email = "vas@gmail.com";
+        emp.phone = "457895";
+        emp.aboutEmployee();
+        System.out.println();
+
+
+        Person[] per = new Person[5];
+
+        per[0] = new Person("Петров Петр Петрович", 30, "Менеджер", "4562222",
+                "petr@gmail.com", 1000);
+        per[1] = new Person("Сидоров Александр Петрович", 55, "Руководитель отдела",
+                "4588888", "sidr@gmail.com", 5000);
+        per[2] = new Person("Николаев Николай Николаевич", 27, "Менеджер",
+                "4589999", "nik@gmail.com", 1100);
+        per[3] = new Person("Васильева Василиса Васильевна", 36, "Бухгалтер",
+                "45757522", "vas@gmail.com", 2000);
+        per[4] = new Person("Алексеев Алексей Алексеевич", 25, "Стажер",
+                "4560002", "alex@gmail.com", 900);
+
+
+        Park.Attraction attraction = new Park.Attraction("Карусель Лошадки", "12.00", 4);
+        Park.Attraction attraction1 = new Park.Attraction("Американские горки", "12.00", 9);
+        Park.Attraction attraction2 = new Park.Attraction("Колесо обозрения", "14.00", 6);
+        attraction.infoAboutAttraction();
+        attraction1.infoAboutAttraction();
+        attraction2.infoAboutAttraction();
+
+
+    }
+}
+
---
+++ Lesson4/Person.java
@@ -0,0 +1,21 @@
+package Lesson4;
+
+public class Person {
+    String nameOfPerson;
+    int ageOfPerson;
+    String positionOfPerson;
+    String phoneOfPerson;
+    String emailOfPerson;
+    int salaryOfPerson;
+
+    public Person(String nameOfPerson, int ageOfPerson, String positionOfPerson, String phoneOfPerson,
+                  String emailOfPerson, int salaryOfPerson) {
+        this.nameOfPerson = nameOfPerson;
+        this.ageOfPerson = ageOfPerson;
+        this.positionOfPerson = positionOfPerson;
+        this.phoneOfPerson = phoneOfPerson;
+        this.emailOfPerson = emailOfPerson;
+        this.salaryOfPerson = salaryOfPerson;
+
+    }
+}
