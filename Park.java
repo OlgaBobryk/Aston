85854aef1058d1fd3d1aa63956d51628fcebe6a9
Olga <aqa_olga.bobryk@mail.com>
2024-11-10 22:43:39 +0300

    added lesson4 into new branch

---
+++ Park.java
@@ -0,0 +1,23 @@
+public class Park {
+
+    private Attraction attriction;
+
+    public static class Attraction {
+
+        String nameOfAttraction;
+        int price;
+        String time;
+
+        public Attraction(String nameOfAttraction,String time, int price){
+            this.nameOfAttraction=nameOfAttraction;
+            this.time=time;
+            this.price=price;
+        }
+        public void infoAboutAttraction(){
+            System.out.println("Атракцион "  + nameOfAttraction + ", " + "время " + time + ", " + "стоимость " +  price
+            + ".");
+
+        }
+
+    }
+}
