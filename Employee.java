0d88936887a3928e4a0c4c05f33b19c6277f5af4
Olga <aqa_olga.bobryk@mail.com>
2024-11-10 22:41:53 +0300

    added lesson4 into new branch

---
+++ Employee.java
@@ -0,0 +1,16 @@
+public class Employee {
+
+    String name;
+    String email;
+    String phone;
+    int salary;
+    int age;
+
+    public void aboutEmployee() {
+        System.out.println("Сотрудник " + " " + name + ", возраст" + " " + age + ". " + "Информация о сотруднике:" +
+                " " + "почта" + " " + email + ";" + " " + "телефон" +
+                " " + phone + ";" + " " + "зп" + " " + salary);
+    }
+
+}
+
