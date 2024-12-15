92464f7198fd0337d240505a75ff626c6f88501c
Olga <aqa_olga.bobryk@mail.com>
2024-12-15 21:26:57 +0300

    Lesson

---
+++ src/test/java/Factorial.java
@@ -0,0 +1,13 @@
+public class Factorial {
+    public int getFactorial(int a) {
+        if (a == 0 || a == 1) {
+            return 1;
+        } else {
+            int result = 1;
+            for (int i = 1; i <= a; i++) {
+                result = result * i;
+            }
+            return result;
+        }
+    }
+}
---
+++ src/test/java/FactorialTest.java
@@ -0,0 +1,19 @@
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+
+
+public class FactorialTest {
+
+    @Test
+    @DisplayName("Факториал числа")
+    @RepeatedTest(2)
+
+    public void factorialOfNumber() {
+        Factorial factorialOfNumber = new Factorial();
+        Assertions.assertEquals(120, factorialOfNumber.getFactorial(5), "Значения отличаются");
+    }
+}
